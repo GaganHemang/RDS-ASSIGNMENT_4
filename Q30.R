ae7d70a44185ecfa1c7ada06918c3d8d3fbc28a1
GaganHemang <167464697+GaganHemang@users.noreply.github.com>
2025-04-24 17:38:28 +0530

    Update Q30.R

--- Q30.R
+++ Q30.R
@@ -1,2 +1,2 @@
-df <- data.frame(Name = c("Revu", "Deppu", "Lily"), Age = c(25,30,35))
-subset(df,Age > 20 & Name=="Deppu")
+df <- data.frame(Name = c("Gagan", "Dilip", "Nithin"), Age = c(25,30,35))
+subset(df,Age > 20 & Name=="Gagan")
